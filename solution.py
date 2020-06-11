268b90a0447d14eb6840e891a4f8dcde2b6bb74d
Rajmani Sharma <54034236+iamraj25@users.noreply.github.com>
2020-06-11 22:36:36 +0530

    Create solution.py

---
+++ solution.py
@@ -0,0 +1,27 @@
+''' Read input from STDIN. Print your output to STDOUT '''
+    #Use input() to read input from STDIN and use print to write your output to STDOUT
+
+def main():
+    T = int(input())
+    N = []
+    teamG = []
+    opponent = []
+    for i in range(T):
+        N.append(int(input()))
+        _teamg = [int(x) for x in input().split()]
+        _teamg = sorted(_teamg)
+        _oppo = [int(x) for x in input().split()]
+        _oppo = sorted(_oppo)
+        teamG.append(_teamg)
+        opponent.append(_oppo)
+    for _ in range(T):
+        win = 0
+        j = 0
+        for i in range(N[_]):
+            if teamG[_][i]>opponent[_][j]:
+                win += 1
+                j += 1
+        print(win)
+
+main()
+
